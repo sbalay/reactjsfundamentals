cb966bda68cdc22f483e607b276aa0465d49f099
Sebastian Balay <snbalay@gmail.com>
2016-04-23 18:19:31 -0300

    first course. first react component

---
+++ app/index.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var app = document.getElementById('app');
+
+var HelloWorld = React.createClass({
+  render: function () {
+    return (
+      <div> Hello World </div>
+    )
+  }
+});
+
+
+ReactDOM.render(
+  <HelloWorld />,
+  app
+);
