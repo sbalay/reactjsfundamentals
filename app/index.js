8b9bf483f473114555c8100e43685e9698e5e357
Sebastian Balay <snbalay@gmail.com>
2016-04-23 18:57:38 -0300

    second course. composable component

--- app/index.js
+++ app/index.js
@@ -4,14 +4,50 @@ var app = document.getElementById('app');
 
 var HelloWorld = React.createClass({
   render: function () {
+    console.log(this.props);
     return (
-      <div> Hello World </div>
+      <div> Hello {this.props.name} </div>
     )
   }
 });
 
+var USER_DATA = {
+  name: 'seba',
+  username: 'sbalay',
+  image: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRPAazN5G9pHFVRuW0Iuv1AclfKU2eW83MHyf52qelP0vquYIYQ8w'
+};
+
+var ProfilePic = React.createClass({
+  render: function () {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function () {
+    return <a href={'https://github.com/' + this.props.username}>{this.props.username}</a>;
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function () {
+    return <div>{this.props.name}</div>;
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    );
+  }
+});
 
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   app
 );
